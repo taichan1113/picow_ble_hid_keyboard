abcf27aa3a48340b6db119d604c36ab679316caa
Taichi Takemoto <takecchi.1113@gmail.com>
2024-01-18 17:44:35 +0900

    modified

--- btstack_hog_kb.c
+++ btstack_hog_kb.c
@@ -206,20 +206,6 @@ static void send_report(int modifier, int keycode)
 
 static btstack_timer_source_t typing_timer;
 
-static int send_keycode;
-static int send_modifier;
-static int send_keyup;
-
-static void send_key(int modifier, int keycode){
-    send_keycode = keycode;
-    send_modifier = modifier;
-    hids_device_request_can_send_now_event(con_handle);
-}
-
-static void typing_can_send_now(void){
-   send_report(send_modifier, send_keycode);
-}
-
 static void typing_timer_handler(btstack_timer_source_t *ts)
 {
     // get keycode and send
@@ -231,18 +217,12 @@ static void typing_timer_handler(btstack_timer_source_t *ts)
     } report_q;
 
     if (queue_try_remove(&hid_keyboard_report_queue, &report_q)){
-        // uint8_t report[] = { report_q.modifier, 0, 
-        //             report_q.keycode[0],report_q.keycode[1],report_q.keycode[2],report_q.keycode[3],report_q.keycode[4],report_q.keycode[5]};
-        // hids_device_send_input_report(con_handle, report, sizeof(report));
-        send_key(report_q.modifier, report_q.keycode);
+        uint8_t report[] = { report_q.modifier, 0, 
+                    report_q.keycode[0],report_q.keycode[1],report_q.keycode[2],report_q.keycode[3],report_q.keycode[4],report_q.keycode[5]};
+        hids_device_send_input_report(con_handle, report, sizeof(report));
     }
 
-    // hids_device_request_can_send_now_event(con_handle);
-
-    // set next timer
-    btstack_run_loop_set_timer(ts, TYPING_PERIOD_MS);
-    btstack_run_loop_add_timer(ts);
-
+    hids_device_request_can_send_now_event(con_handle);
 }
 
 static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packet, uint16_t size)
@@ -291,12 +271,11 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
             printf("Protocol Mode: %s mode\n", hids_subevent_protocol_mode_get_protocol_mode(packet) ? "Report" : "Boot");
             break;
         case HIDS_SUBEVENT_CAN_SEND_NOW:
-            typing_can_send_now();
             // send_report(send_modifier, send_keycode);
             // btstack_run_loop_set_timer_handler(&typing_timer, typing_timer_handler);
             // typing_timer.process = &typing_timer_handler;
-            // btstack_run_loop_set_timer(&typing_timer, TYPING_PERIOD_MS);
-            // btstack_run_loop_add_timer(&typing_timer);
+            btstack_run_loop_set_timer(&typing_timer, TYPING_PERIOD_MS);
+            btstack_run_loop_add_timer(&typing_timer);
 
             break;
         default:
